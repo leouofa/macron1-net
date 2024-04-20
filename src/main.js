8e0c8fcafe24ae7b526bd9433f543173d51b2164
Leonid Medovyy <leonid@macron1.io>
2024-04-20 13:49:47 -0700

    setting up the stage

--- src/main.js
+++ src/main.js
@@ -2,11 +2,63 @@ import kaboom from "kaboom"
 
 const k = kaboom()
 
+k.setGravity(2400)
+
 k.loadSprite("bean", "sprites/bean.png")
 
-k.add([
-	k.pos(120, 80),
-	k.sprite("bean"),
+const player = k.add([
+  k.sprite("bean"),
+  k.scale(0.5),
+  k.pos(20, 20),
+  k.area(),
+  k.body()
 ])
 
-k.onClick(() => k.addKaboom(k.mousePos()))
+const MOVE_SPEED = 200
+k.onKeyDown('right', () => {
+  player.move(MOVE_SPEED, 0)
+})
+
+k.onKeyDown('left', () => {
+  player.move(-MOVE_SPEED, 0)
+})
+
+k.onKeyDown('space', () => {
+  player.jump()
+})
+
+k.addLevel([
+  '                                        ',
+  '                                        ',
+  '                                        ',
+  '                                        ',
+  '                                        ',
+  '                                        ',
+  '                                        ',
+  '         @       @  @         @     @   ',
+  '                                        ',
+  'xx   xxxxxx xxxxxxxxxx xxxxxxxxx xxxxxxx',
+
+], {
+  tileWidth: 40,
+  tileHeight: 40,
+  tiles: {
+    'x': () => [
+      sprite('bean'),
+      area(),
+      body({ isStatic: true })
+    ],
+  }
+})
+
+
+
+// k.loadSprite("bean", "sprites/bean.png")
+//
+// k.add([
+//   k.pos(120, 120),
+//   k.scale(1.5),
+//   k.sprite("bean"),
+// ])
+//
+// k.onClick(() => k.addKaboom(k.mousePos()))
